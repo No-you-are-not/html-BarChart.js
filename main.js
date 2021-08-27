5d699b206ae6175d2a401f23cb5d15aba094a456
sorshireality <polina.doctorwho@gmail.com>
2021-08-27 12:59:29 +0300

    refactored main.js

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 
 var dataJson = {
     2020:{
-        "Jan":1234,
-        "Feb":1357,
-        "Mar":2376,
-        "Apr":2234,
-        "May":1234,
-        "Jun":5078,
-        "Jul":1234,
-        "Aug":5378,
+
         "Sep":1234,
         "Oct":5678,
         "Nov":1234,
@@ -26,10 +19,6 @@ var dataJson = {
 };
 
 function createBarChart(dataJson) {
-    //var colors = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
-    var colorsMeaning = ['very_good', 'good','average', 'worse', 'bad']
-    const TROW = 'tr',
-        TDATA = 'td';
 
     function turnIntoTime(timestamp) {
         var hours = Math.floor(timestamp / 60 / 60);
@@ -44,15 +33,18 @@ function createBarChart(dataJson) {
         return `${hours}:${minutes}:${seconds}`;
     }
 
-   const YEARS = Object.keys(dataJson);
+
+    var colorsMeaning = ['very_good', 'good','average', 'worse', 'bad']
+    const TROW = 'tr';
+    const TDATA = 'td';
+    const YEARS = Object.keys(dataJson);
     let TIME_VALUES = [];
     let MONTH_VALUES = [];
     let dataOfEveryYearsArray = [];
+
    for (let i = 0; i < YEARS.length; i++){
        dataOfEveryYearsArray.push(Object.values(dataJson)[i])
    }
-
-
 
    for (let i = 0; i < dataOfEveryYearsArray.length; i++){
        TIME_VALUES.push(Object.values(dataOfEveryYearsArray[i]));
@@ -60,27 +52,28 @@ function createBarChart(dataJson) {
        MONTH_VALUES.push(Object.keys(dataOfEveryYearsArray[i]));
        MONTH_VALUES = MONTH_VALUES.flat();
    }
+
+
     let mediana = (TIME_VALUES.reduce((a, b) => a + b, 0))/TIME_VALUES.length;
-    const MEDIANA_HEIGHT = Math.floor((mediana*(500*0.9))/Math.max(...TIME_VALUES)) + 50;
-    console.log(MEDIANA_HEIGHT);
-        //Math.floor((500 * (mediana/150))/100);
-    //((dataValues[j]*90)/Math.max(...TIME_VALUES)) + '%';
+    const MEDIANA_HEIGHT = Math.floor((mediana*(500*0.9))/Math.max(...TIME_VALUES)) + 60;
     var medianaContainer = document.createElement('div');
     medianaContainer.setAttribute('class', 'mediana');
     let medianaTime = turnIntoTime(mediana);
     medianaContainer.innerText = medianaTime;
+    var hrLine = document.getElementById('line');
+    hrLine.style.transform = `translateY(-${MEDIANA_HEIGHT}px)`;
+    console.log(MEDIANA_HEIGHT);
 
 
     var chart = document.createElement('div');
     var barchart = document.createElement('table');
     var titlerow = document.createElement(TROW);
     var titledata = document.createElement(TDATA);
-    var hrLine = document.getElementById('line');
     var barrow = document.createElement(TROW);
     titledata.setAttribute('colspan', 'MONTH_VALUES.length');
     barchart.appendChild(titlerow);
     chart.appendChild(barchart);
-    hrLine.style.transform = `translateY(-${MEDIANA_HEIGHT}px)`;
+
 
     for (let i = 0; i < dataOfEveryYearsArray.length; i++){
         let dataKeys = Object.keys(dataOfEveryYearsArray[i]);
@@ -89,16 +82,14 @@ function createBarChart(dataJson) {
             barrow.setAttribute('class', 'bars');
             var bardata = document.createElement(TDATA);
             var bar = document.createElement('div');
-            //bar.setAttribute('class', colors[i]);
 
             if (dataValues[j] == Math.max(...TIME_VALUES)){
                 bar.style.height =  "90%";
-                console.log(bar.style.height);
             }
             else {
                 bar.style.height = ((dataValues[j]*90)/Math.max(...TIME_VALUES)) + '%';
-                console.log(bar.style.height);
             }
+
             bardata.innerText = turnIntoTime(dataValues[j]);
 
             var hours = Math.floor(dataValues[j] / 60 / 60);
@@ -120,11 +111,13 @@ function createBarChart(dataJson) {
                 bar.setAttribute('class', colorsMeaning[4]);
             }
 
-            bardata.appendChild(bar);
-            barrow.appendChild(bardata);
             var barmonth = document.createElement('div');
             barmonth.innerText = dataKeys[j] + ` 202${[i]}`;
+
+            bardata.appendChild(bar);
+            barrow.appendChild(bardata);
             bardata.appendChild(barmonth);
+
         }
     }
     barchart.appendChild(barrow);
